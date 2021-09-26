bb1d87e916bf90e2e1cd5bcc0bb36b032f673c3e
rajithpera <rajith.akalanka.2013@gmail.com>
2021-09-27 01:01:44 +0530

    working draft

--- Shipping cal.py
+++ Shipping cal.py
@@ -7,24 +7,58 @@ F = ['algeria', 'andorra', 'belgium', 'benin', 'burkina faso', 'cote d ivoire', 
 G = ['cape verde', 'fiji', 'gambia', 'guinea bissau', 'guinea rep: of', 'iceland', 'liberia', 'marshall islands', 'mauritania', 'new zealand', 'samoa western', 'senegal', 'sierra leone','syrian arab rep','st.pierre & miquelon','st. pierre & miquelon', 'tuvalu', 'vanuatu','virgin islands(usa)','virgin islands (usa)','wallis and futuna islands']
 H = ['anguilla', 'antigua & barbuda', 'antilles (netherlands)', 'argentina', 'aruba', 'bahamas', 'barbados', 'belize', 'bermuda', 'bolivia', 'brazil', 'canada', 'cayman island', 'chile', 'colombia', 'cook island', 'costa rica', 'cuba', 'dominica', 'dominican rep:', 'ecuador', 'el salvador', 'grenada','guadeloupe', 'guatemala', 'guyana','haiti', 'honduras', 'jamaica', 'kiribati', 'mexico', 'nicaragua', 'niue', 'panama', 'paraguay', 'peru', 'puerto rico', 'samoa', 'st christopher & nevis', 'st lucia', 'st. vincent & grenadines','st.vincent','st. vincent', 'suriname', 'tonga', 'trinidad & tobago', 'u.s.a','usa', 'uruguay', 'venezuela']
 
+Ap = [160,325,600,1150]
+Bp = [185,358,725,1400]
+Cp = [205,445,845,1640]
+Dp = [230,510,970,1895]
+Ep = [255,570,1090,2135]
+Fp = [280,630,1215,2380]
+Gp = [310,700,1355,2665]
+Hp = [425,995,1940,3835]
 
-name = str(input("enter the name: "))
+def country(name):
+    if name.lower() in A:
+        name = ('A')
+    elif name.lower() in B:
+        name = ('B')
+    elif name.lower() in C:
+        name = ('C')
+    elif name.lower() in D:
+        name = ('D')
+    elif name.lower() in E:
+        name = ('E')
+    elif name.lower() in F:
+        name = ('F')
+    elif name.lower() in G:
+        name = ('G')
+    elif name.lower() in H:
+        name = ('H')
+    else:
+        name = ('NOT FOUND')
+    return name
 
-if name.lower() in A:
-    print ('A')
-elif name.lower() in B:
-    print ('B')
-elif name.lower() in C:
-    print ('C')
-elif name.lower() in D:
-    print ('D')
-elif name.lower() in E:
-    print ('E')
-elif name.lower() in F:
-    print ('F')
-elif name.lower() in G:
-    print ('G')
-elif name.lower() in H:
-    print ('H')
-else:
-    print ('NOT FOUND')
+
+# def weightcat(weight,):
+c_name = str(input("enter the name: "))
+weight = float(input("Please enter the weight: "))
+x = (country(c_name))
+
+def main (wei,group):
+    
+    if wei==100:
+        y = int(0)
+    elif 100 < wei <= 250:
+        y = int(1)
+    elif 251 <= wei <= 500:
+        y = int(2)
+    elif 501 <= wei <= 1000:
+        y = int(3)
+
+    p_group = group+'p'
+    var = globals()[p_group]
+        
+    return (var[y])
+
+print(main(weight,x))
+
+
